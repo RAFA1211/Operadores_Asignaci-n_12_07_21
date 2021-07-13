f40d53dd23c40b30fe0ee9f16a8d6859da803b93
RAFA1211 <rafael.angarita.raar@gmail.com>
2021-07-12 21:15:23 -0500

    Este es el ejercicio de operadores de asignación

---
+++ main.js
@@ -0,0 +1,53 @@
+
+//Operador de Suma 
+    
+    let x = 12 
+    let y = 343 
+
+    let z = x + y 
+    console.log(z)
+
+let aviso1=`Buenos Días  `
+let aviso2=`Compañeros Curso Programación`
+
+aviso1 += aviso2
+console.log(aviso1)
+
+// Operador de Sustracción 
+    x = 10 
+    y = 3 
+
+   x -= y
+   let b = (x-y)
+    console.log(x)
+    console.log(b)
+
+    let aviso3=`Buenas Noches `
+    let aviso4= 4 
+
+    aviso3 -= aviso4
+    console.log(aviso3)
+
+// Operador de Multiplicación 
+
+let f = 5 
+let g = 4 
+
+let h= f * g 
+console.log(h)
+
+// Operador de División 
+
+let i=32;
+let j=7;
+let k= i/j
+console.log(k)
+
+// Operador de Resto 
+ i=32
+ j=7
+
+let m= i%j
+console.log(m)
+let n=i**j
+console.log(n)
